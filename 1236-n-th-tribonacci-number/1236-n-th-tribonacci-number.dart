e37223b65403d4cca23ad61886137986d9c2d495
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-08 09:33:38 +0900

    Time: 0 ms (100%), Space: 148.3 MB (69.23%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.dart
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.dart
@@ -1,4 +1,8 @@
 class Solution {
+  /// 2025.07.08 #1137
+  /// n번째 Tribonacci 수 구하기
+  /// Tribonacci 수열은 초기 값이 T0 = 0, T1 = 1, T2 = 1로 시작하며,
+  /// Tn = T(n-1) + T(n-2) + T(n-3)
   int tribonacci(int n) {
     List<int> list = [0, 1, 1];
     if(n < 3) return list[n];
