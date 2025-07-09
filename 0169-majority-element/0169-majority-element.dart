11dc12b4a70f2d5c3c8344b895b4ca864e7d6476
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-09 09:19:04 +0900

    Time: 23 ms (13.01%), Space: 156.2 MB (25.34%) - LeetHub

--- 0169-majority-element/0169-majority-element.dart
+++ 0169-majority-element/0169-majority-element.dart
@@ -1,4 +1,7 @@
 class Solution {
+  /// 2025.07.09 #169
+  /// 배열에서 가장 많이 포함돼있는 값(최빈값) 찾기
+  /// 최빈값은 배열의 원소 중 절반 초과의 개수를 차지하는 원소
   int majorityElement(List<int> nums) {
     if (nums.length == 1) return nums[0];
   
