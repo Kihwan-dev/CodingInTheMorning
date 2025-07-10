e6562fad9e36a6c1298ad998768396ed71e15f50
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-10 09:42:47 +0900

    Time: 0 ms (100%), Space: 151.4 MB (33.99%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.dart
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.dart
@@ -5,9 +5,7 @@ class Solution {
     }
     int idx = 0;
     for(int i=1; i<nums.length; i++) {
-      if(nums[idx] == nums[i]) {
-
-      } else {
+      if(nums[idx] != nums[i]) {
         idx++;
         nums[idx] = nums[i];
       }
