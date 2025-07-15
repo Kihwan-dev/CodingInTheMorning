9b075fa54ec616fd30881407735af78b2f54678e
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-15 09:21:46 +0900

    Time: 4 ms (31.61%), Space: 189.7 MB (67.24%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.dart
@@ -1,4 +1,11 @@
 class Solution {
+  /// 2025.07.15 #121
+  /// 길이 n인 정수 배열 prices
+  /// prices[i]는 i번째 날에 해당 주식의 가격
+  /// 한 번의 거래(한 주를 매수하고, 이후의 다른 하루에 매도)로 얻을 수 있는 최대 이익 구하기
+  /// 이익을 낼 수 없는 경우에는 0 리턴
+  /// Input  : prices = [7,1,5,3,6,4]
+  /// Output : 5
   int maxProfit(List<int> prices) {
     int buy = 99999;
     int sell = -1;
