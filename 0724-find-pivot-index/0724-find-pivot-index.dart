8f3759e15f047fdc67a64c575b00fd8ec1a94b49
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-16 09:25:29 +0900

    Time: 2 ms (71.43%), Space: 149.6 MB (57.14%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,24 +1,19 @@
 class Solution {
 int pivotIndex(List<int> nums) {
   if (nums.length == 1) return 0;
-  int pivotIndex = 0;
+  int sum = 0;
   int leftSum = 0;
-  int rightSum = 0;
 
-  while (pivotIndex < nums.length) {
-    for (int i = 0; i < nums.length; i++) {
-      if (i < pivotIndex) {
-        leftSum += nums[i];
-      } else if (i == pivotIndex) {
-        continue;
-      } else {
-        rightSum += nums[i];
-      }
-    }
-    if(leftSum == rightSum) return pivotIndex;
-    pivotIndex++;
-    leftSum = 0;
-    rightSum = 0;
+
+  for(int n in nums) {
+    sum += n;
+  }
+
+  for(int i=0; i<nums.length; i++) {
+    if(leftSum == (sum - nums[i])/2) {
+      return i;
+    } 
+    leftSum += nums[i];
   }
 
   return -1;
