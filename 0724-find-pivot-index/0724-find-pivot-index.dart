efcdfc3e145f0602c681e21b2de8ff64c5d647d2
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-16 09:57:43 +0900

    Time: 2 ms (71.43%), Space: 147.9 MB (95.24%) - LeetHub

--- 0724-find-pivot-index/0724-find-pivot-index.dart
+++ 0724-find-pivot-index/0724-find-pivot-index.dart
@@ -1,10 +1,10 @@
 class Solution {
-/// 2025.07.16 #724
-/// 정수 배열 nums 피벗 인덱스 찾기
-/// 피벗 인덱스 : 인덱스 i의 왼쪽에 있는 모든 원소의 합과
-///               오른쪽에 있는 모든 원소의 합이 같은 위치
-/// 찾으면 i 리턴 없으면 -1 리턴
-/// 여러개일경우 가장 왼쪽에 있는 인덱스 리턴
+  /// 2025.07.16 #724
+  /// 정수 배열 nums 피벗 인덱스 찾기
+  /// 피벗 인덱스 : 인덱스 i의 왼쪽에 있는 모든 원소의 합과
+  ///               오른쪽에 있는 모든 원소의 합이 같은 위치
+  /// 찾으면 i 리턴 없으면 -1 리턴
+  /// 여러개일경우 가장 왼쪽에 있는 인덱스 리턴
   int pivotIndex(List<int> nums) {
     if (nums.length == 1) return 0;
     int sum = 0;
@@ -16,7 +16,7 @@ class Solution {
     }
   
     for(int i=0; i<nums.length; i++) {
-      if(leftSum == (sum - nums[i])/2) {
+      if (leftSum * 2 == sum - nums[i]) {
         return i;
       } 
       leftSum += nums[i];
