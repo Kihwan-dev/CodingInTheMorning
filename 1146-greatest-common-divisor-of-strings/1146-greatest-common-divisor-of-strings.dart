0919b7f102910ba89cf047d3c25029582606135f
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-18 10:03:11 +0900

    Time: 6 ms (5.56%), Space: 152.2 MB (5.56%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.dart
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.dart
@@ -1,40 +1,44 @@
 class Solution {
-String gcdOfStrings(String str1, String str2) {
-  int longLength;
-  int shortLength;
-  String longStr;
-  String shortStr;
+  /// # 2025.07.18 #1071
+  /// ## 문자열의 최대 공약수
+  /// - 주어진 두 문자열 str1, str2의 최대 공약수 찾기
+  /// - "ABCABC"와 "ABC"는 공통적으로 "ABC"로 나뉠 수 있음
+  String gcdOfStrings(String str1, String str2) {
+    int longLength;
+    int shortLength;
+    String longStr;
+    String shortStr;
 
-  if(str1.length > str2.length) {
-    longLength = str1.length;
-    longStr = str1;
-    shortLength = str2.length;
-    shortStr = str2;
-  } else {
-    longLength = str2.length;
-    longStr = str2;
-    shortLength = str1.length;
-    shortStr = str1;
+    if(str1.length > str2.length) {
+      longLength = str1.length;
+      longStr = str1;
+      shortLength = str2.length;
+      shortStr = str2;
+    } else {
+      longLength = str2.length;
+      longStr = str2;
+      shortLength = str1.length;
+      shortStr = str1;
+    }
+
+    if (!longStr.contains(shortStr)) return "";
+
+    while (shortLength != 0) {
+      int n = shortLength;
+      shortLength = longLength % shortLength;
+      longLength = n;
+    }
+
+    String expectedGcdLetters = shortStr.substring(0, longLength);
+
+    while(true) {
+      if(!longStr.startsWith(expectedGcdLetters)) break;
+      if(longStr == longStr.replaceFirst(expectedGcdLetters, "")) break;
+      longStr = longStr.replaceFirst(expectedGcdLetters, "");
+    }
+    // print(str1);
+    if(longStr.isNotEmpty) return "";
+
+    return expectedGcdLetters;
   }
-
-  if (!longStr.contains(shortStr)) return "";
-
-  while (shortLength != 0) {
-    int n = shortLength;
-    shortLength = longLength % shortLength;
-    longLength = n;
-  }
-
-  String expectedGcdLetters = shortStr.substring(0, longLength);
-
-  while(true) {
-    if(!longStr.startsWith(expectedGcdLetters)) break;
-    if(longStr == longStr.replaceFirst(expectedGcdLetters, "")) break;
-    longStr = longStr.replaceFirst(expectedGcdLetters, "");
-  }
-  // print(str1);
-  if(longStr.isNotEmpty) return "";
-
-  return expectedGcdLetters;
-}
 }
