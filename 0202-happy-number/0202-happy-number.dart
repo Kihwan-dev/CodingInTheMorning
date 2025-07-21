5c7343d3ed32472711bc9569b7bbdc61e35911c9
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-21 09:25:51 +0900

    Time: 4 ms (78.69%), Space: 150.5 MB (36.07%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.dart
@@ -0,0 +1,22 @@
+class Solution {
+  bool isHappy(int n) {
+    
+    List<int> list = [];
+
+    int getEachDigitSquared(int num) {
+      int sum = 0;
+
+      for(int i=0; i<num.toString().length; i++) {
+        sum = sum + (int.parse(num.toString()[i]) * int.parse(num.toString()[i]));
+      }
+
+      if(sum == 1) return sum;
+      if(list.contains(sum)) return 0;
+      list.add(sum);
+
+      return getEachDigitSquared(sum);
+    }
+
+    return getEachDigitSquared(n) == 1;
+  }
+}
