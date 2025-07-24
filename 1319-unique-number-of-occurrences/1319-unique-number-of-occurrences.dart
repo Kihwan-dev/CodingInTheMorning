f1c78bf7c0e11e9e62c964c4510b3d28c8d92549
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-24 09:22:24 +0900

    Time: 13 ms (25%), Space: 151.7 MB (25%) - LeetHub

---
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.dart
@@ -0,0 +1,13 @@
+class Solution {
+bool uniqueOccurrences(List<int> arr) {
+  if(arr.length == 1) return true;
+
+  Map<int, int> map = {};
+
+  for(var n in arr) {
+    map[n] = (map[n] ?? 0) + 1;
+  }
+
+  return map.values.toList().join(" ") == map.values.toList().toSet().join(" ");
+}
+}
