b13c7361ea58783c151a54a414b754453d3832ab
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-24 09:33:58 +0900

    Time: 6 ms (79.17%), Space: 149 MB (58.33%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.dart
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.dart
@@ -1,29 +1,33 @@
 class Solution {
-// bool uniqueOccurrences(List<int> arr) {
-//   if(arr.length == 1) return true;
-
-//   Map<int, int> map = {};
-
-//   for(var n in arr) {
-//     map[n] = (map[n] ?? 1) + 1;
-//   }
-
-//   return map.values.length == map.values.toSet().length;
-// }
-bool uniqueOccurrences(List<int> arr) {
-  if(arr.length == 1) return true;
-
-  Map<int, int> map = {};
-  Set<int> oSet = {};
-
-  for(var n in arr) {
-    map[n] = (map[n] ?? 1) + 1;
-  }
+  /// # 2025.07.24 #1207
+  /// ## 고유한 빈도 횟수
+  /// 정수 배열 arr
+  /// arr의 각 원소가 나타나는 빈도 수가 고유한지 확인
+  /// 빈도가 모두 다르면 true 리턴, 아니면 false 리턴
+  /// input [1,2,2,1,1,3] / output true
+  /// 1 - 3번 / 2 - 2번 / 3 - 1 번
+  bool uniqueOccurrences(List<int> arr) {
+    if(arr.length == 1) return true;
   
-  for(var n in map.values) {
-    if(!oSet.add(n)) return false;
-  }
+    Map<int, int> map = {};
 
-  return true;
-}
+    
+    Set<int> oSet = {};
+  
+    for(var n in arr) {
+      map[n] = (map[n] ?? 1) + 1;
+    }
+    
+    for(var n in map.values) {
+      if(!oSet.add(n)) return false;
+    }
+  
+    return true;
+  
+    // for(var n in arr) {
+    //   map[n] = (map[n] ?? 1) + 1;
+    // }
+  
+    // return map.values.length == map.values.toSet().length;
+  }
 }
