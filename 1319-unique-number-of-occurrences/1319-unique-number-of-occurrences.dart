edfed5e6f4f3f6a0b58b8965bd43b0b735b8bde0
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-24 09:29:24 +0900

    Time: 6 ms (79.17%), Space: 151.2 MB (29.17%) - LeetHub

--- 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.dart
+++ 1319-unique-number-of-occurrences/1319-unique-number-of-occurrences.dart
@@ -1,13 +1,29 @@
 class Solution {
+// bool uniqueOccurrences(List<int> arr) {
+//   if(arr.length == 1) return true;
+
+//   Map<int, int> map = {};
+
+//   for(var n in arr) {
+//     map[n] = (map[n] ?? 1) + 1;
+//   }
+
+//   return map.values.length == map.values.toSet().length;
+// }
 bool uniqueOccurrences(List<int> arr) {
   if(arr.length == 1) return true;
 
   Map<int, int> map = {};
+  Set<int> oSet = {};
 
   for(var n in arr) {
     map[n] = (map[n] ?? 1) + 1;
   }
+  
+  for(var n in map.values) {
+    if(!oSet.add(n)) return false;
+  }
 
-  return map.values.length == map.values.toSet().length;
+  return true;
 }
 }
