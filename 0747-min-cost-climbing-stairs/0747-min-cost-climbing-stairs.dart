eca1bddb34fd0ab23d70ef412c9327ff980611f8
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-25 10:01:31 +0900

    Time: 1 ms (100%), Space: 148.9 MB (83.33%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.dart
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.dart
@@ -1,4 +1,11 @@
 class Solution {
+  /// # 2025.07.25 #746
+  /// ## 계단을 올라가는 최소 비용
+  /// 계단 배열 cost -> 계단을 올라가는 최소 비용을 계산
+  /// 각 요소는 해당 계단을 밟을 때의 비용
+  /// 계단은 한 번에 한 계단 또는 두 계단씩 오를 수 있음
+  /// 배열의 마지막이나 마지막에서 두 번째 인덱스에서 도착하는 것이 목표
+  /// input [1, 100, 1, 1, 1, 100, 1, 1, 100, 1] output 6
   int minCostClimbingStairs(List<int> cost) {
     if (cost.length == 2) return min(cost[0], cost[1]);
 
@@ -7,9 +14,9 @@ class Solution {
     root.add(cost[1]);
 
     for (int i = 2; i < cost.length; i++) {
-      root.add(cost[i] + min(root[i-1], root[i-2]));
+      root.add(cost[i] + min(root[i - 1], root[i - 2]));
     }
 
-    return min(root[cost.length-1], root[cost.length-2]);
+    return min(root[cost.length - 1], root[cost.length - 2]);
   }
 }
