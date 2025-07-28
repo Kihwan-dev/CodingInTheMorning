0af1e874422062fd17596a35cd7aeb4b830e465c
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-28 10:17:05 +0900

    Time: 199 ms (44.44%), Space: 188.1 MB (77.78%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.dart
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.dart
@@ -1,3 +1,10 @@
+/// # 2025.07.28 #933
+/// ## 최근 호출 수
+/// 클래스 RecentCount 구현
+/// 주어진 시간 범위 내에서 호출 수를 기록하고 리턴
+/// RecenteCounter()는 RecentCounter 객체를 초기화
+/// int ping(int t)는 t시점에서 호출됐음을 기록,
+/// [t-3000, t] 범위 내에서 발생한 모든 호출 수 리턴
 class RecentCounter {
 
   List<int> records = [];
@@ -8,7 +15,6 @@ class RecentCounter {
     records.add(t);
     int count = 0;
     for (var record in records) {
-      // if(t - 3000 <= record && record <= t) count++;
       if(record < t - 3000) {
         count++;
       } else {
