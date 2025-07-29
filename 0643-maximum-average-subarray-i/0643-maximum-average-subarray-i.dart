30e3658c8361b9ff3b859a2bef989484d64d6eb3
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-29 10:06:03 +0900

    Time: 3 ms (56.1%), Space: 172.4 MB (97.56%) - LeetHub

--- 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.dart
+++ 0643-maximum-average-subarray-i/0643-maximum-average-subarray-i.dart
@@ -1,19 +1,24 @@
 class Solution {
-double findMaxAverage(List<int> nums, int k) {
-  if (nums.length == 1) return double.parse(nums[0].toStringAsFixed(5));
+  /// # 2025.07.29 #643
+  /// ## 최대 평균 하위 배열 1
+  /// 정수 배열 nums, 정수 k
+  /// nums에서 길이가 k인 부분배열의 평균값 중 최대값
+  /// 소수점 다섯 번째 자리까지 반올림하여 리턴
+  double findMaxAverage(List<int> nums, int k) {
+    if (nums.length == 1) return double.parse(nums[0].toStringAsFixed(5));
 
-  int sum = 0;
+    int sum = 0;
 
-  for (int i = 0; i < k; i++) {
-    sum += nums[i];
+    for (int i = 0; i < k; i++) {
+      sum += nums[i];
+    }
+    int maxSum = sum;
+
+    for (int i = k; i < nums.length; i++) {
+      sum += nums[i];
+      sum -= nums[i - k];
+      maxSum = max(maxSum, sum);
+    }
+    return double.parse((maxSum / k).toStringAsFixed(5));
   }
-  int maxSum = sum;
-
-  for (int i = k; i < nums.length; i++) {
-    sum += nums[i];
-    sum -= nums[i - k];
-    maxSum = max(maxSum, sum);
-  }
-  return double.parse((maxSum / k).toStringAsFixed(5));
-}
 }
