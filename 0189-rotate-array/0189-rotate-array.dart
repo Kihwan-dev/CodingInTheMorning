9d79dbdaa945edff93da398e810c89bf22e05222
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-30 09:44:30 +0900

    Time: 5 ms (27.78%), Space: 160.1 MB (33.33%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.dart
+++ 0189-rotate-array/0189-rotate-array.dart
@@ -1,16 +1,20 @@
 class Solution {
-void rotate(List<int> nums, int k) {
-  if(nums.length == 1 || nums.length == k) return;
-
-  if(k > nums.length) {
-    k %= nums.length;
+  /// # 2025.07.30 #189
+  /// ## 배열 회전
+  /// 정수 배열 nums 오른쪽으로 k번 회전
+  /// 회전 : 마지막 요소를 배열의 처음으로 이동
+  void rotate(List<int> nums, int k) {
+    if(nums.length == 1 || nums.length == k) return;
+    
+    if(k > nums.length) {
+        k %= nums.length;
+    }
+    
+    List<int> list = [];
+    list.addAll(nums.sublist(nums.length - k));
+    list.addAll(nums.sublist(0, nums.length - k));
+    
+    nums.clear();
+    nums.addAll(list);
   }
-
-  List<int> list = [];
-  list.addAll(nums.sublist(nums.length - k));
-  list.addAll(nums.sublist(0, nums.length - k));
-
-  nums.clear();
-  nums.addAll(list);
-}
 }
