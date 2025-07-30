a74d92b6a6962181785675dbd86d61131e16fa48
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-07-30 09:42:38 +0900

    Time: 5 ms (27.78%), Space: 162.8 MB (7.41%) - LeetHub

---
+++ 0189-rotate-array/0189-rotate-array.dart
@@ -0,0 +1,17 @@
+class Solution {
+void rotate(List<int> nums, int k) {
+  if(nums.length == 1 || nums.length == k) return;
+
+  if(k > nums.length) {
+    k %= nums.length;
+    print(k);
+  }
+
+  List<int> list = [];
+  list.addAll(nums.sublist(nums.length - k));
+  list.addAll(nums.sublist(0, nums.length - k));
+
+  nums.clear();
+  nums.addAll(list);
+}
+}
