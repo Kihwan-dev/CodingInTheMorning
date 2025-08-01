fdf01e359ee3b36a2352c54872ab4fd8213a8b06
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-01 09:43:37 +0900

    Time: 376 ms (40%), Space: 152.4 MB (32%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.dart
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.dart
@@ -1,42 +1,48 @@
 class Solution {
   int removeDuplicates(List<int> nums) {
     /* #1
-      if (nums.length <= 2) return nums.length;
-      int currentNum = -10001;
-      bool isExist = false;
-      for (int i = 0; i < nums.length; i++) {
-        int n = nums[i];
-        if (currentNum < n) {
-          currentNum = n;
-          isExist = true;
-        } else {
-          if (isExist) {
-            isExist = false;
+        if (nums.length <= 2) return nums.length;
+        int currentNum = -10001;
+        bool isExist = false;
+        for (int i = 0; i < nums.length; i++) {
+          int n = nums[i];
+          if (currentNum < n) {
+            currentNum = n;
+            isExist = true;
           } else {
-            nums.removeAt(i);
-            i--;
+            if (isExist) {
+              isExist = false;
+            } else {
+              nums.removeAt(i);
+              i--;
+            }
           }
         }
-      }
-      return nums.length;
-      */
+        return nums.length;
+        */
 
     /*#2 */
     if (nums.length < 3) return nums.length;
 
     // int loopPoint = 0;
     int checkPoint = 0;
+    List<int> answer = [nums[0]];
 
     for (int i = 1; i < nums.length; i++) {
-      if (nums[i] == nums[checkPoint]) {
+      int n = nums[i];
+      if (n == nums[checkPoint]) {
         if (i - checkPoint > 1) {
-          nums.removeAt(i);
-          i--;
+          //
+        } else {
+          answer.add(n);
         }
       } else {
+        answer.add(n);
         checkPoint = i;
       }
     }
+    nums.clear();
+    nums.addAll(answer);
     return nums.length;
   }
 }
