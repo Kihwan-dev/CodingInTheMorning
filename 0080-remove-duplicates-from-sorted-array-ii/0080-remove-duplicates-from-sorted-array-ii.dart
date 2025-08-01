c8ac9aec5c7c67ebc211fb8f3a5a74c85e628762
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-01 10:57:03 +0900

    Time: 375 ms (44%), Space: 151.5 MB (56%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.dart
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.dart
@@ -5,6 +5,29 @@ class Solution {
   /// 각 요소가 최대 2번 반복까지 남기고 제거
   /// 최종 배열의 길이 리턴
   int removeDuplicates(List<int> nums) {
+    /* 
+      // #1 778ms
+      if (nums.length <= 2) return nums.length;
+      int currentNum = -10001;
+      bool isExist = false;
+      for (int i = 0; i < nums.length; i++) {
+        int n = nums[i];
+        if (currentNum < n) {
+          currentNum = n;
+          isExist = true;
+        } else {
+          if (isExist) {
+            isExist = false;
+          } else {
+            nums.removeAt(i);
+            i--;
+          }
+        }
+      }
+      return nums.length;
+      */
+
+    // #1-2 344ms
     if (nums.length <= 2) return nums.length;
     int currentNum = nums[0];
     bool isExist = true;
@@ -27,5 +50,31 @@ class Solution {
     nums.clear();
     nums.addAll(answer);
     return nums.length;
+
+    /*
+      // #2 376ms
+      if (nums.length < 3) return nums.length;
+
+      // int loopPoint = 0;
+      int checkPoint = 0;
+      List<int> answer = [nums[0]];
+
+      for (int i = 1; i < nums.length; i++) {
+        int n = nums[i];
+        if (n == nums[checkPoint]) {
+          if (i - checkPoint > 1) {
+            //
+          } else {
+            answer.add(n);
+          }
+        } else {
+          answer.add(n);
+          checkPoint = i;
+        }
+      }
+      nums.clear();
+      nums.addAll(answer);
+      return nums.length;
+      */
   }
 }
