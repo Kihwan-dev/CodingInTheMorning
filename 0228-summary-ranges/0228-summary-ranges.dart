00e6da94b9e831d2568556725f8e0b565a4f85a5
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-04 10:17:25 +0900

    Time: 1 ms (100%), Space: 150 MB (43.33%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.dart
+++ 0228-summary-ranges/0228-summary-ranges.dart
@@ -1,4 +1,9 @@
 class Solution {
+  /// # 2025.08.04 #288
+  /// ## 요약 범위
+  /// 정렬된 고유 정수 배열 nums
+  /// 연속된 숫자들을 하나의 범위로 묶어 문자열 형태로 리턴
+  /// 법위는 "a->b" 형태로 하나의 숫자는 "a" 형태
   List<String> summaryRanges(List<int> nums) {
     if (nums.isEmpty) return [];
     if (nums.length == 1) return [nums[0].toString()];
@@ -19,6 +24,7 @@ class Solution {
         start = nums[i];
         end = nums[i];
       }
+
       if (i == nums.length - 1) {
         if (start != end) {
           answer.add("$start->$end");
