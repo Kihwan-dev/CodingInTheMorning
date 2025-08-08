c84242ff4a71917c9f5258ffccc34797600ec674
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-08 09:40:54 +0900

    Time: 1984 ms (11.87%), Space: 175.8 MB (93.22%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
@@ -3,7 +3,7 @@ bool containsNearbyDuplicate(List<int> nums, int k) {
   for (int i = 0; i < nums.length; i++) {
     for (int j = i + 1; j < nums.length; j++) {
       if (j - i > k) break;
-      if (nums[i] == nums[j] && (i - j).abs() <= k) {
+      if (nums[i] == nums[j] && j-i<= k) {
         return true;
       }
     }
