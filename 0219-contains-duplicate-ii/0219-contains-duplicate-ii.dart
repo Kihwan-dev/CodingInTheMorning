7c2f8abdc8a2f0b9b36a7a2cd49cf3a60fddbdd9
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-08 09:55:26 +0900

    Time: 42 ms (74.58%), Space: 198.7 MB (22.03%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
@@ -1,12 +1,26 @@
 class Solution {
 bool containsNearbyDuplicate(List<int> nums, int k) {
-  for (int i = 0; i < nums.length; i++) {
-    for (int j = i + 1; j < nums.length; j++) {
+  //
+  //
+
+  /* for (int i = 0; i < min(nums.length, i+k); i++) {
+    for (int j = i + 1; j < min(nums.length, j+k); j++) {
       if (j - i > k) break;
-      if (nums[i] == nums[j] && j-i<= k) {
+      if (nums[i] == nums[j] && j - i <= k) {
         return true;
       }
     }
+  }
+  return false; */
+
+  Map<int, int> map = {};
+  for (int i = 0; i < nums.length; i++) {
+    int key = nums[i];
+    if (map.containsKey(key)) {
+      int value = map[key]!;
+      if (i - value <= k) return true;
+    }
+    map[key] = i;
   }
   return false;
 }
