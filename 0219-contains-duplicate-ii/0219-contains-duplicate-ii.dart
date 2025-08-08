8cfa4fb240df79008d7a92adb4296df5e47a9be2
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-08 09:55:50 +0900

    Time: 39 ms (81.36%), Space: 195.8 MB (52.54%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.dart
@@ -1,27 +1,33 @@
 class Solution {
-bool containsNearbyDuplicate(List<int> nums, int k) {
-  //
-  //
+  /// # 2025.08.08 #219
+  /// ## 중복된 값 포함 2
+  /// 정수배열 nums, 정수 k
+  /// 같은 값을 가지는 두 인덱스 i와 j에 대해
+  /// abs(i - j) <= k를 만족하는 경우가 있는지 확인(abs : 절댓값)
+  /// 있으면 true, 없으면 false 리턴
+  bool containsNearbyDuplicate(List<int> nums, int k) {
+    //
+    //
 
-  /* for (int i = 0; i < min(nums.length, i+k); i++) {
-    for (int j = i + 1; j < min(nums.length, j+k); j++) {
-      if (j - i > k) break;
-      if (nums[i] == nums[j] && j - i <= k) {
-        return true;
+    /* for (int i = 0; i < min(nums.length, i+k); i++) {
+      for (int j = i + 1; j < min(nums.length, j+k); j++) {
+        if (j - i > k) break;
+        if (nums[i] == nums[j] && j - i <= k) {
+          return true;
+        }
       }
     }
-  }
-  return false; */
+    return false; */
 
-  Map<int, int> map = {};
-  for (int i = 0; i < nums.length; i++) {
-    int key = nums[i];
-    if (map.containsKey(key)) {
-      int value = map[key]!;
-      if (i - value <= k) return true;
+    Map<int, int> map = {};
+    for (int i = 0; i < nums.length; i++) {
+      int key = nums[i];
+      if (map.containsKey(key)) {
+        int value = map[key]!;
+        if (i - value <= k) return true;
+      }
+      map[key] = i;
     }
-    map[key] = i;
+    return false;
   }
-  return false;
-}
 }
