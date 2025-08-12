420cc781b9e03e91dda88d34f4d44fb832b981f1
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-12 09:19:59 +0900

    Time: 1 ms (92.86%), Space: 149 MB (46.43%) - LeetHub

--- 0290-word-pattern/0290-word-pattern.dart
+++ 0290-word-pattern/0290-word-pattern.dart
@@ -1,21 +1,30 @@
 class Solution {
-bool wordPattern(String pattern, String s) {
-  //
-  Map<String, String> map = {};
-  final words = s.split(" ");
-  if (pattern.length != words.length) return false;
-  for (int i = 0; i < pattern.length; i++) {
+  /// # 2025.08.12 #290
+  /// ## 단어 패턴
+  /// 문자열 pattern, s
+  /// pattern의 각 문자가 s의 단어에 매핑될 수 있는지 확인
+  /// 1 <= pattern.length <= 300
+  /// 1 <= s.length <= 3000
+  /// ex) pattern : "abba", s = "dog cat cat dog" => return true
+  /// ex) pattern : "abba", s = "dog cat cat fish" => return false
+  /// ex) pattern : "abba", s = "dog dog dog dog" => return false
+  bool wordPattern(String pattern, String s) {
     //
-    if (map.containsKey(pattern[i])) {
-      if (map[pattern[i]] != words[i]) {
+    Map<String, String> map = {};
+    final words = s.split(" ");
+    if (pattern.length != words.length) return false;
+    for (int i = 0; i < pattern.length; i++) {
+      //
+      if (map.containsKey(pattern[i])) {
+        if (map[pattern[i]] != words[i]) {
+          return false;
+        }
+      } else if (map.containsValue(words[i])) {
         return false;
+      } else {
+        map[pattern[i]] = words[i];
       }
-    } else if (map.containsValue(words[i])) {
-      return false;
-    } else {
-      map[pattern[i]] = words[i];
     }
+    return true;
   }
-  return true;
-}
 }
