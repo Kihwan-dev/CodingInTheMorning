e6acdbfe49862446be6e43c76406d2973c529c89
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-21 09:38:59 +0900

    Time: 3 ms (88.62%), Space: 152.4 MB (24.87%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.dart
+++ 0013-roman-to-integer/0013-roman-to-integer.dart
@@ -1,24 +1,35 @@
 class Solution {
 int romanToInt(String s) {
-  Map<String, int> map = {
-    "I": 1,
-    "V": 5,
-    "X": 10,
-    "L": 50,
-    "C": 100,
-    "D": 500,
-    "M": 1000,
-  };
-  if (s.length == 1) return map[s]!;
-  int answer = 0;
-  for (int i = 0; i < s.length - 1; i++) {
-    if (map[s[i + 1]]! > map[s[i]]!) {
-      answer -= map[s[i]]!;
-    } else {
-      answer += map[s[i]]!;
+  int getNum(String str) {
+    switch (str) {
+      case 'I':
+        return 1;
+      case 'V':
+        return 5;
+      case 'X':
+        return 10;
+      case 'L':
+        return 50;
+      case 'C':
+        return 100;
+      case 'D':
+        return 500;
+      case 'M':
+        return 1000;
+      default:
+        return 0;
     }
   }
-  answer += map[s[s.length - 1]]!;
+
+  if (s.length == 1) return getNum(s);
+  int answer = getNum(s[0]);
+  for (int i = 1; i < s.length; i++) {
+    answer += getNum(s[i]);
+    if (getNum(s[i]) > getNum(s[i - 1])) {
+      answer -= getNum(s[i - 1]);
+      answer -= getNum(s[i - 1]);
+    }
+  }
   return answer;
 }
 }
