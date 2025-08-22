831f6c7e87396770208548c76b08f1ff78f8eadc
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-22 10:31:07 +0900

    Time: 3 ms (52.94%), Space: 155.9 MB (47.06%) - LeetHub

--- 0055-jump-game/0055-jump-game.dart
+++ 0055-jump-game/0055-jump-game.dart
@@ -2,13 +2,14 @@ class Solution {
 bool canJump(List<int> nums) {
   if (nums.length == 1) return true;
   if (nums[0] == 0) return false;
-  int maxIndex = 0;
+  int maxIndex = -1;
   for (int i = 0; i < nums.length; i++) {
-    if (i > maxIndex) {
-      return false;
+    if (i == maxIndex) {
+      if (nums[i] == 0) return false;
     }
+    if (nums[i] + i >= nums.length - 1) return true;
     maxIndex = max(maxIndex, nums[i] + i);
   }
-  return true;
+  return false;
 }
 }
