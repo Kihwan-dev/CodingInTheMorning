da1ef9eb7663b055c5feac9faa2f2b22d76f404c
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-25 09:39:17 +0900

    Time: 0 ms (100%), Space: 148.8 MB (60.53%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.dart
+++ 0206-reverse-linked-list/0206-reverse-linked-list.dart
@@ -7,16 +7,28 @@
  * }
  */
 class Solution {
-ListNode? reverseList(ListNode? head) {
-  if (head == null) return null;
-  ListNode? current = head;
-  ListNode? next;
+  /// # 2025.08.25 #206
+  /// ## Reverse Linked List
+  /// * 문제
+  /// 단일 연결(Linked) 리스트의 헤드가 주어졌을 때, 리스트를 역순으로 뒤집으세요.
+  /// class ListNode {
+  ///   int val;
+  ///   ListNode? next;
+  ///   ListNode([this.val = 0, this.next]);
+  /// }
+  /// * 조건
+  /// 1. 연결 리스트의 노드 개수는 0≤n≤5000 입니다.
+  /// 2. 각 노드는 -5000 <= Node.val <= 5000의 정수 값을 가집니다.
+  ListNode? reverseList(ListNode? head) {
+    if (head == null) return null;
+    ListNode? current = head;
+    ListNode? next;
 
-  while (current != null) {
-    next = ListNode(current.val, next);
-    current = current.next;
+    while (current != null) {
+      next = ListNode(current.val, next);
+      current = current.next;
+    }
+
+    return next;
   }
-
-  return next;
-}
 }
