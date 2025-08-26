d5ba6d6258396e1c590f637ea4d73055755a5b84
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-26 09:59:18 +0900

    Time: 28 ms (23.53%), Space: 149.3 MB (70.59%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.dart
@@ -0,0 +1,22 @@
+class Solution {
+ List<int> countBits(int n) {
+  List<int> ans = [];
+
+  int getBin(int num) {
+    int cnt = 0;
+
+    while (num != 0) {
+      if (num % 2 == 1) cnt++;
+      num = (num / 2).toInt();
+    }
+
+    return cnt;
+  }
+
+  for (int i = 0; i < n + 1; i++) {
+    ans.add(getBin(i));
+  }
+
+  return ans;
+}
+}
