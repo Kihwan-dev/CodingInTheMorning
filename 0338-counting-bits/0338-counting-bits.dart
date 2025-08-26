c1f100ef3462d39b230afd6dd2c32eefb05fd7e9
Kihwan_dev <63238519+Kihwan-dev@users.noreply.github.com>
2025-08-26 10:04:06 +0900

    Time: 9 ms (41.18%), Space: 151.3 MB (41.18%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.dart
+++ 0338-counting-bits/0338-counting-bits.dart
@@ -1,22 +1,30 @@
 class Solution {
- List<int> countBits(int n) {
-  List<int> ans = [];
+  /// # 2025.08.26 #338
+  /// ## 비트 개수 구하기
+  /// * 문제
+  ///   정수 n이 주어졌을 때, 길이가 n + 1인 배열을 반환하세요.
+  ///   배열의 각 원소는 index의 이진수 표현에서 1의 개수를 나타냅니다.
+  ///   (단, 0 <= i <= n)
+  /// * 조건
+  ///   0 <= n <= 10^5
+  List<int> countBits(int n) {
+    List<int> ans = [];
 
-  int getBin(int num) {
-    int cnt = 0;
+    int getBin(int num) {
+      int cnt = 0;
 
-    while (num != 0) {
-      if (num % 2 == 1) cnt++;
-      num ~/= 2;
+      while (num != 0) {
+        if (num % 2 == 1) cnt++;
+        num ~/= 2;
+      }
+
+      return cnt;
     }
 
-    return cnt;
-  }
+    for (int i = 0; i < n + 1; i++) {
+      ans.add(getBin(i));
+    }
 
-  for (int i = 0; i < n + 1; i++) {
-    ans.add(getBin(i));
+    return ans;
   }
-
-  return ans;
-}
 }
